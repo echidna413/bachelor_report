f612844daa64b2853ff1df828f9e2cac7813bf7c
Diana <di.sinenkova@gmail.com>
2019-06-04 22:59:20 +0400

    Обзор технологий в процессе

---
+++ Диплом/Context.cs
@@ -0,0 +1,15 @@
+﻿using Domain.Core;
+using Domain.Core.Logs;
+using System.Data.Common;
+using System.Data.Entity;
+
+namespace Infrastructure.Data
+{
+    public class Context : DbContext
+    {
+        
+        static Context() => Database.SetInitializer(new Initializer());
+
+        public Context(string connectionString) : base(connectionString) { }
+    }
+}
---
+++ Диплом/Initializer.cs
@@ -0,0 +1,16 @@
+﻿using Domain.Core;
+using Infrastructure.Data.Utilities;
+using System;
+using System.Data.Entity;
+using System.IO;
+
+namespace Infrastructure.Data
+{
+    internal class Initializer : DropCreateDatabaseIfModelChanges<Context>
+    {
+        protected override void Seed(Context database)
+        {
+            //Тут инициализируется БД
+        }
+    }
+}
